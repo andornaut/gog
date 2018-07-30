416e0d49dcaf354bea68627e111dcd345ad39245
andornaut <andornaut@users.noreply.github.com>
2018-07-30 13:50:56 -0400

    Switch from urfave/cli to spf13/cobra

--- cmd/repository.go
+++ cmd/repository.go
@@ -9,76 +9,117 @@ import (
 
 	"github.com/andornaut/gog/git"
 	"github.com/andornaut/gog/repository"
+	"github.com/spf13/cobra"
 )
 
-// RunAddRepository runs the `gog repository add` command
-func RunAddRepository(repoName string, repoURL string) error {
-	if err := repository.ValidateRepoName(repoName); err != nil {
-		return err
-	}
+var isPath bool
 
-	repoPath := path.Join(repository.BaseDir, repoName)
-	if err := os.MkdirAll(repoPath, os.ModePerm); err != nil {
-		return err
-	}
+var repositoryCmd = &cobra.Command{
+	Use:          "repository [command]",
+	Short:        "Manage repositories",
+	SilenceUsage: true,
+}
 
-	if repoURL == "" {
-		if err := git.Init(repoPath); err != nil {
+var addRepositoryCmd = &cobra.Command{
+	Use:   "add [name] [url]",
+	Short: "Add a git repository",
+	Args:  cobra.RangeArgs(1, 2),
+	DisableFlagsInUseLine: true,
+	RunE: func(c *cobra.Command, args []string) error {
+		repoName := args[0]
+		repoURL := ""
+		if len(args) > 1 {
+			repoURL = args[1]
+		}
+
+		if err := repository.ValidateRepoName(repoName); err != nil {
 			return err
 		}
-	} else {
-		if err := git.Clone(repoPath, repoURL); err != nil {
+
+		repoPath := path.Join(repository.BaseDir, repoName)
+		if err := os.MkdirAll(repoPath, os.ModePerm); err != nil {
 			return err
 		}
-	}
-	fmt.Println(repoPath)
-	return nil
-}
 
-// RunRemoveRepository runs the `gog repository remove` command
-func RunRemoveRepository(repoName string) error {
-	if err := repository.ValidateRepoName(repoName); err != nil {
-		return err
-	}
-
-	repoPath := path.Join(repository.BaseDir, repoName)
-	if err := os.RemoveAll(repoPath); err != nil {
-		return err
-	}
-	fmt.Println(repoPath)
-	return nil
-}
-
-// RunGetDefaultRepository runs the `gog repository get-default` command
-func RunGetDefaultRepository(isPath bool) error {
-	repoPath, err := repository.GetDefault()
-	if err != nil {
-		return err
-	}
-
-	if isPath {
-		fmt.Println(repoPath)
-	} else {
-		fmt.Println(filepath.Base(repoPath))
-	}
-	return nil
-}
-
-// RunListRepositories runs the `gog repository list` command
-func RunListRepositories(isPath bool) error {
-	entries, err := ioutil.ReadDir(repository.BaseDir)
-	if err != nil {
-		return err
-	}
-
-	for _, fileInfo := range entries {
-		if fileInfo.IsDir() {
-			msg := fileInfo.Name()
-			if isPath {
-				msg = path.Join(repository.BaseDir, msg)
+		if repoURL == "" {
+			if err := git.Init(repoPath); err != nil {
+				return err
 			}
-			fmt.Println(msg)
+		} else {
+			if err := git.Clone(repoPath, repoURL); err != nil {
+				return err
+			}
 		}
-	}
-	return nil
+		fmt.Println(repoPath)
+		return nil
+	},
+}
+
+var removeRepositoryCmd = &cobra.Command{
+	Use:   "remove [name]",
+	Short: "Remove a repository",
+	Args:  cobra.ExactArgs(1),
+	DisableFlagsInUseLine: true,
+	RunE: func(c *cobra.Command, args []string) error {
+		repoName := args[0]
+		if err := repository.ValidateRepoName(repoName); err != nil {
+			return err
+		}
+		repoPath := path.Join(repository.BaseDir, repoName)
+		if err := os.RemoveAll(repoPath); err != nil {
+			return err
+		}
+		fmt.Println(repoPath)
+		return nil
+	},
+}
+
+var getDefaultRepositoryCmd = &cobra.Command{
+	Use:   "get-default [--path]",
+	Short: "Print the name of the default repository",
+	Args:  cobra.NoArgs,
+	DisableFlagsInUseLine: true,
+	RunE: func(c *cobra.Command, args []string) error {
+		repoPath, err := repository.GetDefault()
+		if err != nil {
+			return err
+		}
+
+		if isPath {
+			fmt.Println(repoPath)
+		} else {
+			fmt.Println(filepath.Base(repoPath))
+		}
+		return nil
+	},
+}
+
+var listRepositoryCmd = &cobra.Command{
+	Use:   "list [--path]",
+	Short: "Print the names of all repositories",
+	Args:  cobra.NoArgs,
+	DisableFlagsInUseLine: true,
+	RunE: func(c *cobra.Command, args []string) error {
+		entries, err := ioutil.ReadDir(repository.BaseDir)
+		if err != nil {
+			return err
+		}
+
+		for _, fileInfo := range entries {
+			if fileInfo.IsDir() {
+				msg := fileInfo.Name()
+				if isPath {
+					msg = path.Join(repository.BaseDir, msg)
+				}
+				fmt.Println(msg)
+			}
+		}
+		return nil
+	},
+}
+
+func init() {
+	getDefaultRepositoryCmd.Flags().BoolVarP(&isPath, "path", "p", false, "print the path of the default repository")
+	listRepositoryCmd.Flags().BoolVarP(&isPath, "path", "p", false, "print the paths of all repositories")
+	repositoryCmd.AddCommand(addRepositoryCmd, removeRepositoryCmd, getDefaultRepositoryCmd, listRepositoryCmd)
 }
